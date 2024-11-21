533c9bec0244b7305d160cb7dca663d2b0def132
Luis Alexander Borjas Zamora <119633920+alex22bo@users.noreply.github.com>
2024-11-21 13:35:39 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,51 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["my", "your", "his", "their", "their", "her", "its", "our"];
+  let adjective = [
+    "arrogant",
+    "brave",
+    "charming",
+    "cooperative",
+    "courageous",
+    "determined",
+    "elegant",
+    "funny"
+  ];
+  let noun = [
+    "history",
+    "family",
+    "theory",
+    "ability",
+    "problem",
+    "variety",
+    "week",
+    "policy"
+  ];
+  let extension = [
+    ".com",
+    ".es",
+    ".ve",
+    ".gov",
+    ".ong",
+    ".net",
+    ".edu",
+    ".cat"
+  ];
+  let newDomains = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adjective.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extension.length; l++) {
+          newDomains += `<li class="list-group">${pronoun[i]}${adjective[j]}${noun[k]}${extension[l]}</li>`;
+        }
+      }
+    }
+  }
+
+  document.querySelector("#dominios").innerHTML = newDomains;
 };
